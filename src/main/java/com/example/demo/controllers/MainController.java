8223b5904db792ce1bb8b5e90ab0bbbb9ac24d71
Sen4ek <51993614+Sen4ek@users.noreply.github.com>
2020-07-22 20:19:06 +0700

    Test address

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,11 +1,19 @@
 package com.example.demo.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
+
     @GetMapping
-    public String main() {
+    public String main(Model model, HttpServletRequest request)
+    {
+        model.addAttribute("address", request.getRemoteAddr());
         return "main";
     }
 }
