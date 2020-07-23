0094434315310258d6ad4b5792c230ea4dc713b7
Sen4ek <51993614+Sen4ek@users.noreply.github.com>
2020-07-23 19:06:58 +0700

    Requests

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,8 +1,12 @@
 package com.example.demo.controllers;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 import javax.servlet.http.HttpServletRequest;
@@ -19,4 +23,34 @@ public class MainController {
                 r.getLocalAddr(), r.getLocalPort(), r.getLocalName()));
         return "main";
     }
+
+
+    @GetMapping("/ping")
+    public String ping(Model model) {
+        model.addAttribute("request", "ping");
+        model.addAttribute("type", "Наш запрос");
+        return "requests";
+    }
+
+    @GetMapping("/subscribe")
+    public String subscribe(Model model) {
+        model.addAttribute("request", "subscribe");
+        model.addAttribute("type", "Наш запрос");
+        return "requests";
+    }
+
+    @GetMapping("/unsubscribe")
+    public String unsubscribe(Model model) {
+        model.addAttribute("request", "unsubscribe");
+        model.addAttribute("type", "Наш запрос");
+        return "requests";
+    }
+
+    @GetMapping("/request/{data}")
+    public String request(Model model, @PathVariable String data) {
+        model.addAttribute("request", data);
+        model.addAttribute("type", "Не наш запрос");
+        return "requests";
+    }
+
 }
